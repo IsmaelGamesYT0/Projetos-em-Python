10544519844c5ef079974e7c88ce54d63bde44b1
IsmaelGamesYT0 <126985370+IsmaelGamesYT0@users.noreply.github.com>
2024-10-27 17:48:50 -0300

    quiz.py
    Modificação nas Palavras 'farcry' para 'far cry'.

--- quiz.py
+++ quiz.py
@@ -9,25 +9,24 @@ try:
     print('Vamos então começar!')
     time.sleep(1)
 
-    
-    farcry = input('Qual Farcry é considerado o melhor de todos os tempos?: ')
+    farcry = input('Qual Far Cry é considerado o melhor de todos os tempos?: ')
     farcry = farcry.lower().replace(" ", "")
     print('-' * 70)
-    
-    if farcry.lower() == 'farcry3':
+
+    if farcry == 'farcry3':
         print('Certa Resposta!')
-    elif farcry.lower() == 'farcry2':
-        print('Quase você acertou, Far cry 2 é considerado o 6° Melhor.')
-    elif farcry.lower() == 'farcry5':
-        print('Passou perto, o Farcry 5 é considerando o 2° Melhor.')
-    elif farcry.lower() == 'farcry4':
-        print('Quase, o Farcry 4 é considerando o 3° Melhor.')
-    elif farcry.lower() == 'farcry6':
-        print('Errou. o Farcry 6 é considerado o 4° Melhor.')
-    elif farcry.lower() == 'farcryprimal':
-        print('Passou Longe. o Farcry Primal é considerado o 5° Melhor.')
-    elif farcry.lower() == 'farcry1':
-        print('Desculpe, o Farcry 1 é considerando o 6° Melhor.')
+    elif farcry == 'farcry2':
+        print('Quase você acertou, Far Cry 2 é considerado o 6° Melhor.')
+    elif farcry == 'farcry5':
+        print('Passou perto, o Far Cry 5 é considerado o 2° Melhor.')
+    elif farcry == 'farcry4':
+        print('Quase, o Far Cry 4 é considerado o 3° Melhor.')
+    elif farcry == 'farcry6':
+        print('Errou. o Far Cry 6 é considerado o 4° Melhor.')
+    elif farcry == 'farcryprimal':
+        print('Passou Longe. o Far Cry Primal é considerado o 5° Melhor.')
+    elif farcry == 'farcry1':
+        print('Desculpe, o Far Cry 1 é considerado o 6° Melhor.')
         time.sleep(2)
     else:
         print('Não entendi.')
